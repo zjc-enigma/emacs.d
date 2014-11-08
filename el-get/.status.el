a5bcc7ffeb41b8b23dc8b7aa430e3d7bc9127f26
zjc-enigma <enigma@pku.edu.cn>
2014-11-08 23:42:25 +0800

    add some short cut

--- el-get/.status.el
+++ el-get/.status.el
@@ -1,8 +1,27 @@
-((htmlize status "installed" recipe
+((cl-lib status "installed" recipe
+         (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (cssh status "installed" recipe
+       (:name cssh :website "https://github.com/dimitri/cssh" :description "ClusterSSH meets Emacs " :type github :pkgname "dimitri/cssh" :features cssh))
+ (git-emacs status "installed" recipe
+            (:name git-emacs :description "Yet another git emacs mode for newbies" :type github :pkgname "tsgates/git-emacs" :features git-emacs))
+ (git-modes status "installed" recipe
+            (:name git-modes :description "GNU Emacs modes for various Git-related files" :type github :pkgname "magit/git-modes"))
+ (htmlize status "installed" recipe
           (:type github :pkgname "emacsmirror/htmlize" :name htmlize :website "http://www.emacswiki.org/emacs/Htmlize" :description "Convert buffer text and decorations to HTML." :type emacsmirror :localname "htmlize.el"))
+ (magit status "installed" recipe
+        (:name magit :website "https://github.com/magit/magit#readme" :description "It's Magit! An Emacs mode for Git." :type github :pkgname "magit/magit" :depends
+               (cl-lib git-modes)
+               :info "." :compile "magit.*.el\\'" :build
+               `(("make" "docs"))
+               :build/berkeley-unix
+               (("gmake docs"))
+               :build/windows-nt
+               (progn nil)))
  (metaweblog status "installed" recipe
              (:name metaweblog :description "Metaweblog" :type github :pkgname "punchagan/metaweblog"))
  (org-jekyll status "installed" recipe
              (:name org-jekyll :description "Blog from org-mode using jekyll" :type github :pkgname "juanre/org-jekyll" :features org-jekyll))
+ (ssh status "installed" recipe
+      (:name ssh :website "http://www.splode.com/~friedman/software/emacs-lisp/#ssh" :description "This is a comint-based interface for connecting to remote hosts via ssh." :type http :url "http://www.splode.com/~friedman/software/emacs-lisp/src/ssh.el"))
  (xml-rpc-el status "installed" recipe
              (:name xml-rpc-el :description "An elisp implementation of clientside XML-RPC" :type bzr :url "lp:xml-rpc-el")))
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,10 +1,10 @@
 ;; my customize file
-(add-to-list 'load-path "~/Git/emacs.d/lib/ESS/lisp")
+(add-to-list 'load-path "~/Git/emacs.d/libs/ESS/lisp")
 (load "ess-site")
 
-(add-to-list 'load-path "~/Git/emacs.d/lib/el-get")
+(add-to-list 'load-path "~/Git/emacs.d/libs/el-get")
 (load "el-get")
-(add-to-list 'el-get-recipe-path "~/Git/emacs.d/lib/el-get/recipes")
+(add-to-list 'el-get-recipe-path "~/Git/emacs.d/libs/el-get/recipes")
 (el-get 'sync)
 
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
@@ -18,13 +18,21 @@
 
 (require 'linum)
 (global-linum-mode 1)
-(provide 'init-local)
+
+
+(add-to-list 'load-path "~/Git/emacs.d/libs/tramp/lisp/")
+(require 'tramp)
+(setq tramp-default-method "ssh")
+(setq tramp-default-user "root"
+      tramp-default-host "182.92.235.126")
+
+
 
 (blink-cursor-mode (- (*) (*) (*)))
 (setq desktop-restore-frames nil)
 (desktop-save-mode 1)
 
-(setq load-path (cons "~/Git/emacs.d/lib/org2blog/" load-path))
+(setq load-path (cons "~/Git/emacs.d/libs/org2blog/" load-path))
 (load "org2blog-autoloads")
 (load "xml-rpc")
 
@@ -39,3 +47,16 @@
         ("my-blog"
          :url "http://enigmaspace.org/xmlrpc.php"
          :username "infinite")))
+
+(setq process-coding-system-alist '(("R.*" . utf-8)))
+
+(global-set-key (kbd "M-i")  'split-window-horizontally)
+(global-set-key (kbd "M-o")  'next-multiframe-window)
+(global-set-key (kbd "C-o")  'previous-multiframe-window)
+(global-set-key (kbd "C-x g")  'goto-line)
+(global-set-key (kbd "M-m") 'delete-other-windows)
+
+(global-set-key (kbd "M-h")  "->")
+(global-set-key (kbd "M-u")  "<-")
+
+(provide 'init-local)
