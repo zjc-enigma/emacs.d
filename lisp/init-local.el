560f4f9cd8db59fff785d64a7bbe120a8eb9b8c2
zjc-enigma <enigma@pku.edu.cn>
2015-11-18 23:31:12 +0800

    pig conf

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -100,12 +100,9 @@
         ;; this will make sure spaces are used instead of tabs
         indent-tabs-mode nil)
   ;; we like auto-newline and hungry-delete
-  (c-toggle-auto-hungry-state 1)
-  ;; key bindings for all supported languages.  We can put these in
-  ;; c-mode-base-map because c-mode-map, c++-mode-map, objc-mode-map,
-  ;; java-mode-map, idl-mode-map, and pike-mode-map inherit from it.
-  (define-key c-mode-base-map "/C-m" 'c-context-line-break))
+  (c-toggle-auto-hungry-state 1))
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+(load-file "/Users/Patrick/.emacs.d/site-lisp/piglatin-mode/piglatin.el")
 
 (provide 'init-local)
